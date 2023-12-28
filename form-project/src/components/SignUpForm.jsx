47f9332686e355bb11bce5fc3f46e6f07a1d2375
iicakeii <marccharles243@gmail.com>
2023-12-28 14:35:52 -0500

    SigupForm looks completed

--- form-project/src/components/SignUpForm.jsx
+++ form-project/src/components/SignUpForm.jsx
@@ -5,15 +5,36 @@ const SignUpForm = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState(null);
+    const API_URL = "https://fsa-jwt-practice.herokuapp.com/signup";
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log("here")
+
+        try {
+           const resp = await fetch(API_URL,
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        username: username,
+                        password: password,
+                    })
+
+                });
+           const res = resp.json();
+           console.log(res);
+
+        } catch (e) {
+            setError(e.message);
+        }
     }
 
     return (
         <>
             <h2 className="text-amber-200">Sign Up</h2>
+            {error && <p>{error}</p>}
             <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
                 <label>
                     Username:
