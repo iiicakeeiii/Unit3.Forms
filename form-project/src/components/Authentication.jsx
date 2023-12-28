c7db8708d998fb8d745eb0bc1298d729e7379c7e
iicakeii <marccharles243@gmail.com>
2023-12-27 20:12:19 -0500

    tailwind css

---
+++ form-project/src/components/Authentication.jsx
@@ -0,0 +1,7 @@
+const Authentication = () => {
+    return (
+        <div className="text-3xl text-blue-950">Authenticate</div>
+    )
+}
+
+export default Authentication
---
+++ form-project/src/components/SignUp.jsx
@@ -0,0 +1,7 @@
+const SignUp = () => {
+    return (
+        <div className="text-amber-950">Sign Up</div>
+    )
+}
+
+export default SignUp;
