0d791b1a690a53398993543d8efaf5baf0714bc7
iicakeii <marccharles243@gmail.com>
2023-12-28 20:23:22 -0500

    Authenticate update

--- form-project/src/components/Authenticate.jsx
+++ form-project/src/components/Authenticate.jsx
@@ -1,14 +1,36 @@
+import {useState} from "react";
+
 const Authenticate = ({token, API_URL}) => {
+    const AUTH_URL = `${API_URL}/authenticate`
+    const [successMessage, setSuccessMessage] = useState(null);
+
+    const handleClick = async () => {
+        try {
+            const resp = await fetch(AUTH_URL,
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            );
+            const res = await resp.json();
+            // setSuccessMessage(res.message);
+            console.log(res)
+        } catch (e) {
+            console.error(e.message);
+        }
+    }
     return (
         <>
             <div className="flex flex-col text-blue-300">
                 Authenticate
-                <button onClick={async () => {
-                    const response = await fetch(API_URL);
-                    const res = await response.json();
-                }}>
+                <button onClick={handleClick}>
                     Authenticate Token
                 </button>
+
+                <span>{successMessage}</span>
             </div>
 
         </>
